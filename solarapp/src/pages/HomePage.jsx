aae32853aabe32797b8aaf80660e17a271a28d51
icimz8 <yjha75@gmail.com>
2024-11-19 03:11:27 +0900

    Add header and reset CSS setup 19nov2024

--- solarapp/src/pages/HomePage.jsx
+++ solarapp/src/pages/HomePage.jsx
@@ -8,21 +8,21 @@ import PreFooterImage from "../components/PreFooterImage";
 import Footer from "../components/Footer";
 
 const HomePage = () => {
-    return (
-      <>
-        <h1>Home PAGE - <br />To be editted</h1>
+  return (
+    <>
+      {/* <h1>Home PAGE - <br />To be editted</h1> */}
 
-        <Header />
-        <IconInfoBar />
-        <ScheduleFormSection />
-        <SearchAppointmnetSection />
-        <PreFooterImage />
-        <Footer />
+      <Header />
+      <IconInfoBar />
+      <ScheduleFormSection />
+      <SearchAppointmnetSection />
+      <PreFooterImage />
+      <Footer />
 
-        <h3>Made by - <br />V52 Tier 01 - team 1</h3>
-        <b>Soumyajit. Yojeong. Amanda</b>
-      </>
-    );
-  };
-  
-  export default HomePage;
+      <h3>Made by - <br />V52 Tier 01 - team 1</h3>
+      <b>Soumyajit. Yojeong. Amanda</b>
+    </>
+  );
+};
+
+export default HomePage;
