1ec671b5f6261a658701ddca2ba525af4f0babbc
icimz8 <yjha75@gmail.com>
2024-11-30 00:43:06 +0900

    Update styles and markup structure 30nov2024

--- solarapp/src/components/IconInfoBar.jsx
+++ solarapp/src/components/IconInfoBar.jsx
@@ -7,37 +7,41 @@ const IconInfoBar = () => {
     <div id="icon-info-bar">
       <div class="icon-info-card" id="card001">
         <div class="icon-card-image">
-          <img src="/assets/testimage.png" id="card001Img" alt="card001"/>
+          <img src="/assets/card-01.png" id="card001Img" alt="card001" />
         </div>
         <div class="icon-card-text">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, in!</p>
+          <span>Citywide Coverage</span>
+          <p>Serving all Los Angeles neighborhoods.</p>
         </div>
       </div>
 
       <div class="icon-info-card" id="card002">
         <div class="icon-card-image">
-          <img src="/assets/testimage.png" id="card002Img" alt="card002"/>
+          <img src="/assets/card-02.png" id="card002Img" alt="card002" />
         </div>
         <div class="icon-card-text">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, in!</p>
+          <span>Free Solar Assessment</span>
+          <p>No cost expert evaluation.</p>
         </div>
       </div>
 
       <div class="icon-info-card" id="card003">
         <div class="icon-card-image">
-          <img src="/assets/testimage.png" id="card003Img" alt="card003"/>
+          <img src="/assets/card-03.png" id="card003Img" alt="card003" />
         </div>
         <div class="icon-card-text">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, in!</p>
+          <span>Energy Bill Reduction</span>
+          <p>Lower your monthly expenses.</p>
         </div>
       </div>
 
       <div class="icon-info-card" id="card004">
         <div class="icon-card-image">
-          <img src="/assets/testimage.png" id="card004Img" alt="card004"/>
+          <img src="/assets/card-04.png" id="card004Img" alt="card004" />
         </div>
         <div class="icon-card-text">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, in!</p>
+          <span> Eco-Friendly Solutions</span>
+          <p>Transition to clean, sustainable solar power.</p>
         </div>
       </div>
     </div>
