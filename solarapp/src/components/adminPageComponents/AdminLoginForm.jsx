f0d74c103420f6891ab961f3ea8f8bef0881ef38
icimz8 <yjha75@gmail.com>
2024-12-09 20:24:48 +0900

    Update admin login page 09dec2024

--- solarapp/src/components/adminPageComponents/AdminLoginForm.jsx
+++ solarapp/src/components/adminPageComponents/AdminLoginForm.jsx
@@ -5,12 +5,25 @@ import InvalidCredNotify from "./InvalidCredNotify"
 import '../../styling/adminlogin_styling/AdminLoginForm.css';
 
 const AdminLoginForm = () => {
-    return (
-      <div id="adminLoginForm">
-        <p>AdminLoginForm - to be eddited</p>
-        <InvalidCredNotify />
+  return (
+    <form id="adminLoginForm" className="admin-login-form">
+      <div className="admin-login-header">
+        <h1>Admin Login</h1>
       </div>
-    );
-  };
-  
-  export default AdminLoginForm;
+      <fieldset>
+        <div className="login-form-question">
+          <label htmlFor="input-adminName">Username</label>
+          <input type="text" id="input-adminName" required />
+        </div>
+        <div className="login-form-question">
+          <label htmlFor="input-password">Password</label>
+          <input type="text" id="input-password" required />
+        </div>
+        <InvalidCredNotify />
+        <button type="submit">Login</button>
+      </fieldset>
+    </form>
+  );
+};
+
+export default AdminLoginForm;
